971b4d9f2c6fd13d5c301c5cf564f6b08b055f22
Blueby <andychao.ac@gmail.com>
2015-06-17 21:56:21 -0700

    Update
    -typewriter upgrade!! multiplicative upgrades had me stumped for a
    while... :(
    -changes to how (larger) numbers are shown
    -lots of shit i probably forgot

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // Code goes here
-var credits = 0;
-var wordChoice;
-var wordSelect = "FUCK";
-var result = "FUCK";
-var creditValue = 0;
 
 function onLoad() {
   randWord();
   creditLoad();
 }
+
+var wordChoice;
+var wordSelect = "FUCK";
+var result = "FUCK";
 
 function wordCheck() {
   result = document.getElementById("textbox").value;
@@ -16,7 +15,8 @@ function wordCheck() {
     var a = document.getElementById("creditSpan");
     creditAmount();
     setText(a, credits + creditValue);
-    credits = credits + creditValue;
+    credits = (credits + creditValue);
+    totalCredits = totalCredits = creditValue;
     document.getElementById("textbox").value = "";
     document.getElementById("wrongWord").innerHTML = "";
     randWord();
@@ -28,25 +28,9 @@ function wordCheck() {
   }
 }
 
-function textBox() {
-  result = document.getElementById("textbox").value;
-  typeCredit();
-}
-
-function typeCredit() {
-  if (result == wordSelect) {
-    var a = document.getElementById("creditSpan");
-    creditAmount();
-    setText(a, credits + creditValue);
-    credits = credits + creditValue;
-    document.getElementById("textbox").value = "";
-    randWord();
-  }
-}
-
 function randWord() {
   wordChoice = Math.floor((Math.random() * 10) + 1);
-  if (credits <= 1000) {
+  if (totalCredits <= 750) {
     switch (wordChoice) {
       case 1:
         wordSelect = "aid";
@@ -81,7 +65,7 @@ function randWord() {
       default:
         wordSelect = "ERROR ERROR Default Case"
     }
-  } else if (credits <= 1000000) {
+  } else if (totalCredits <= 1000000) {
     switch (wordChoice) {
       case 1:
         wordSelect = "apples";
@@ -118,39 +102,113 @@ function randWord() {
 
     }
   }
-  document.getElementById("test").innerHTML = "Type this word: " + wordSelect;
+  document.getElementById("test").innerHTML = "Type: " + wordSelect;
 }
 
+var credits = 0;
+var totalCredits = 0;
+var creditValue = 0;
+
 function creditAmount() {
-  creditValue = (wordSelect.length * 10)
+  if (typewriters > 0) {
+    creditValue = ((wordSelect.length * 10) * typewriterMult);
+  } else {
+    creditValue = (wordSelect.length * 10);
+  }
 }
 
 function gainCredit() {
-  credits = (credits + 100);
+  credits = (credits + 10000);
+  totalCredits = (totalCredits + 10000);
   updateCreditText();
 }
 
 function updateCreditText() {
   var a = document.getElementById("creditSpan");
-  setText(a, credits);
+  /* Billion */
+  if (credits >= 1000000000) {
+    var credBil = (credits / 1000000000);
+    setText(a, (credBil.toFixed(2) + " Billion"));
+  }
+  /* Million */
+  else if (credits >= 1000000) {
+    var credMil = (credits / 1000000);
+    setText(a, (credMil.toFixed(2) + " Million"));
+  }
+  /* Thousand */
+  else if (credits >= 1000) {
+    var credTh = (credits / 1000);
+    setText(a, (credTh.toFixed(2) + " Thousand"));
+  } else {
+    setText(a, credits.toFixed());
+  }
 }
 
 function setText(dest, text) {
   dest.replaceChild(document.createTextNode(text), dest.lastChild);
 }
 
+/* Word Upgrades */
+
+var wordLevel = 0;
+
+function wordUpgrade() {
+
+
+}
+
+/* Typewriter Upgrades */
+
+var typewriters = 0;
+var typewriterCost = 1000;
+
+function upTypewriter() {
+  if (credits >= typewriterCost) {
+    ++typewriters;
+    var a = document.getElementById("typewriterSpan");
+    var b = document.getElementById("creditSpan");
+    credits = (credits - typewriterCost);
+    typewriterCost = (typewriterCost * 5);
+    typewriterEffect();
+    setText(a, typewriterMult);
+    setText(b, credits);
+    document.getElementById("typeButton").innerHTML = "Upgrade Typewriter (2x): " + typewriterCost.toFixed() + " Credits";
+  }
+}
+
+var typewriterMult = 0;
+
+function typewriterEffect() {
+  if (typewriters > 1) {
+    typewriterMult = Math.pow(typewriterMult, 2);
+  } else if (typewriters == 1) {
+    typewriterMult = 2;
+  } else {
+    typewriterMult = 1;
+  }
+}
+
+/* Per Second Buyables */
+
 var secretary = 0;
 var secretaryCost = 100;
 
 function addSecretary() {
+  var a = document.getElementById("secretarySpan");
+  var b = document.getElementById("creditSpan");
   if (credits >= secretaryCost) {
-    var a = document.getElementById("secretarySpan");
-    var b = document.getElementById("creditSpan");
+    ++secretary;
     credits = (credits - secretaryCost);
-    secretaryCost = Math.floor(secretaryCost * 1.1);
-    setText(a, ++secretary);
+    secretaryCost = (secretaryCost * 1.1);
+    setText(a, secretary);
     setText(b, credits);
-    document.getElementById("secretaryButton").innerHTML = "Hire Secretary (1 CpS): " + secretaryCost + " Credits";
+    document.getElementById("secretaryButton").innerHTML = "Hire Secretary (1 CpS): " + secretaryCost.toFixed() + " Credits";
+    if (secretary >= 1000000) {
+      var secretaryMil = (secretary / 1000000);
+      setText(a, (secretaryMil.toFixed(2) + "Mil"));
+    } else {
+      setText(a, secretary);
+    }
   }
 }
 
@@ -158,16 +216,24 @@ var company = 0;
 var companyCost = 250;
 
 function addCompany() {
+  var a = document.getElementById("companySpan");
+  var b = document.getElementById("creditSpan");
   if (credits >= companyCost) {
-    var a = document.getElementById("companySpan");
-    var b = document.getElementById("creditSpan");
+    ++company;
     credits = (credits - companyCost);
-    companyCost = Math.floor(companyCost * 1.1);
-    setText(a, ++company);
+    companyCost = (companyCost * 1.1);
     setText(b, credits);
-    document.getElementById("companyButton").innerHTML = "Start a company (10 CpS): " + companyCost + " Credits";
+    document.getElementById("companyButton").innerHTML = "Start a company (10 CpS): " + companyCost.toFixed() + " Credits";
+  }
+  if (company >= 1000000) {
+    var companyMil = (company / 1000000);
+    setText(a, (companyMil.toFixed(2) + "Mil"));
+  } else {
+    setText(a, company);
   }
 }
+
+/* Calculations */
 
 function creditCalc() {
   secretaryPS();
@@ -177,18 +243,20 @@ function creditCalc() {
 
 function secretaryPS() {
   var secCredits = 0;
-  secCredits = (secretary * 1);
+  secCredits = (secretary * 1) / 100;
   credits = (credits + secCredits);
+  totalCredits = (totalCredits + secCredits);
 }
 
 function companyPS() {
   var companyCredits = 0;
-  companyCredits = (company * 10);
+  companyCredits = (company * 10) / 100;
   credits = (credits + companyCredits);
+  totalCredits = (credits + companyCredits);
 }
 
 function creditLoad() {
   var testVar = setInterval(function() {
     creditCalc()
-  }, 1000);
+  }, 10);
 }
